3aa29b20e4c810b734a5817b44c4221bcfadb90d
Reece <Reecesantana@gmail.com>
2023-03-28 00:20:26 -0400

    Directories were good but some linting errors and type security

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Head from "next/head";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loadingSpinner";
-
-import { api, RouterOutputs } from "~/utils/api";
+import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import Link from "next/link";
@@ -23,7 +22,7 @@ const CreatePostWizard = () => {
   const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
       setInput("")
-      ctx.posts.getAll.invalidate()
+      void ctx.posts.getAll.invalidate()
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content
